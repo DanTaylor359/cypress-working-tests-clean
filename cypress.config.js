b37ec96190229f6f6502b841f94326ebd00aa579
DanTaylor359 <taylor359@btinternet.com>
2026-03-25 17:50:35 +0000

    Add .gitignore and remove node_modules from tracking

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // no special node events needed for Mochawesome
+      return config;
     },
   },
 });
